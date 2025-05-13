5ca41768ac4c602876817e139d21faf1bf03faa9
Trim21 <trim21.me@gmail.com>
2025-05-13 10:04:41 +0800

    try

---
+++ recipe/build.py
@@ -0,0 +1,17 @@
+import shlex
+import os
+import sys
+import subprocess
+
+subprocess.check_call(
+    [
+        "cmake",
+        *shlex.split(os.getenv("CMAKE_ARGS")),
+        '-D', 'ENABLE_TLS=1',
+        *(['-D', 'ENABLE_RDMA=1'] if sys.platform == 'linux' else []),
+        '-G', 'Ninja',
+        '.',
+    ]
+)
+
+subprocess.check_call(["ninja", '-v', 'install'])
