26fa109f4f17ff1e7a5f28131cccc7275b4de596
Trim21 <trim21.me@gmail.com>
2025-05-13 10:36:26 +0800

    update

--- recipe/build.py
+++ recipe/build.py
@@ -6,6 +6,7 @@ import json
 from pathlib import Path
 
 print(json.dumps(dict(os.environ), ensure_ascii=False, indent=2, sort_keys=True))
+print(json.dumps(shlex.split(os.environ["CMAKE_ARGS"]), ensure_ascii=False, indent=2, sort_keys=True))
 
 subprocess.check_call(
     [
